692ced8f517d44eefb6862e15d65a951dd7bdc49
joeaba <77398477+joeaba@users.noreply.github.com>
2021-09-12 20:32:02 -0500

    Update service-env-warehouse.sh

--- service-env-warehouse.sh
+++ service-env-warehouse.sh
@@ -1,4 +1,4 @@
 # Availability zone used to referece other files
-ZONE=us-ny5
+ZONE=<availability_zone>
 # Cloud Storage bucket name to store your hourly snapshots
-STORAGE_BUCKET=mainnet-beta-ledger
+STORAGE_BUCKET=<cloud_storage_bucket_name>
